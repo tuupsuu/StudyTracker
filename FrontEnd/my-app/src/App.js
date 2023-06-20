360992f376728121c445454a817180e02a7773f4
eepehukk <eemil.hukkanen01@gmail.com>
2023-06-20 13:27:05 +0300

    Fixed a security risk

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -4,20 +4,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import StudentView from './views/StudentView';
 import TeacherView from './views/TeacherView';
+import { AuthProvider } from './components/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-      <>
+      <AuthProvider>
         <Router> 
           <div className="App">
             <Routes>
               <Route path="/" element={<Login />} />
-              <Route path="/student" element={<StudentView />} />
-              <Route path="/teacher" element={<TeacherView />} />
+              <Route path="/student" element={<PrivateRoute><StudentView /></PrivateRoute>} />
+              <Route path="/teacher" element={<PrivateRoute><TeacherView /></PrivateRoute>} />
             </Routes>
           </div>
         </Router>
-      </> 
+      </AuthProvider> 
   );
 }
 
