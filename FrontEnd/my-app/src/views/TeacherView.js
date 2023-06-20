3d074fb8a0c2f28321c4220c8f44e3428ebadcbe
eepehukk <eemil.hukkanen01@gmail.com>
2023-06-20 09:44:55 +0300

    Opettajan näkökulman tekemistä prototyyppiin

--- FrontEnd/my-app/src/views/TeacherView.js
+++ FrontEnd/my-app/src/views/TeacherView.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './TeacherView.css';
+import photo from '../components/graphOfGrades.jpg';
 
-class TeacherView extends React.Component {
-  // state and methods
+function TeacherView() {
+  const [isSidebarOpen, setSidebarOpen] = React.useState(false);
+  const navigate = useNavigate();
 
-  render() {
-    return (
-      <div style={{ textAlign: 'center', marginTop: '100px' }}>
-        <h1>Welcome, Teacher!</h1>
-        <p>You can evaluate students' tests and view results from this page.</p>
-      </div>
-    );
+  const handleLogout = () => {
+    navigate('..');
   }
+
+  return (
+    <div className="teacher-view">
+      <header className="header">
+        <div className="hamburger" onClick={() => setSidebarOpen(true)} />
+        <h2>Welcome, teacher!</h2>
+        <button className="logout-button" onClick={handleLogout}>Log out</button>
+      </header>
+
+      {isSidebarOpen && (
+        <aside className="sidebar">
+          <button className="close-button" onClick={() => setSidebarOpen(false)}>Close</button>
+          <ul>
+            <li>Create a test</li>
+            <li>Evaluate tests</li>
+            <li>Examine results</li>
+          </ul>
+        </aside>
+      )}
+
+      <section className="content">
+        <div className="alert">Some of the tests are not yet evaluated!</div>
+        <img src={photo} alt="Graph of Grades" />
+      </section>
+    </div>
+  );
 }
 
 export default TeacherView;
