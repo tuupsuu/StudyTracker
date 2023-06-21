c87aa9ecd411a1216b15f1209de16ec99f88d182
jkhaimi <jesse.haimi@icloud.com>
2023-06-21 09:27:09 +0300

    Start test page skeleton created

--- FrontEnd/my-app/src/views/StudentView.js
+++ FrontEnd/my-app/src/views/StudentView.js
@@ -12,37 +12,37 @@ class StudentView extends React.Component {
 
   render() {
     return (
-      <div className='Container'>
-        <div className='TopBar'>
+      <div className="Container">
+        <div className="TopBar">
           <Clock />
-          <div className='TextContainer'>
-            <h1 className='Title'>Welcome, Student 1!</h1>
+          <div className="TextContainer">
+            <h1 className="Title">Welcome, Student 1!</h1>
             <p>You can choose your test from this page.</p>
           </div>
-          <Link to='..' className='LogoutButton'>
+          <Link to=".." className="LogoutButton">
             Logout
           </Link>
         </div>
-        <Link to='./StartTest' className='TestContainerButton'>
-          <div className='TestContainer'>
-            <h2 className='TestContainerTitle' style={{ backgroundColor: this.getRandomColor() }}>
+        <Link to="/start-test" className="TestContainerButton">
+          <div className="TestContainer">
+            <h2 className="TestContainerTitle" style={{ backgroundColor: this.getRandomColor() }}>
               Math test 2nd grade Autumn
             </h2>
-            <div className='TestContainerInfo'>
-              <p className='InfoText'>Teacher: Bill Gates</p>
-              <p className='InfoText'>20.6 10.15 - 20.6 14.15</p>
+            <div className="TestContainerInfo">
+              <p className="InfoText">Teacher: Bill Gates</p>
+              <p className="InfoText">20.6 10.15 - 20.6 14.15</p>
             </div>
           </div>
         </Link>
 
-        <Link to='./StartTest' className='TestContainerButton'>
-          <div className='TestContainer'>
-            <h2 className='TestContainerTitle' style={{ backgroundColor: this.getRandomColor() }}>
+        <Link to="/start-test" className="TestContainerButton">
+          <div className="TestContainer">
+            <h2 className="TestContainerTitle" style={{ backgroundColor: this.getRandomColor() }}>
               Finnish writing test 2nd grade Winter
             </h2>
-            <div className='TestContainerInfo'>
-              <p className='InfoText'>Teacher: Antti Valmari</p>
-              <p className='InfoText'>Requires password</p>
+            <div className="TestContainerInfo">
+              <p className="InfoText">Teacher: Antti Valmari</p>
+              <p className="InfoText">Requires password</p>
             </div>
           </div>
         </Link>
