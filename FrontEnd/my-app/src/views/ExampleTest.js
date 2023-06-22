ff552a6b1effe8dbf8fe8a808fa3403d525f6de0
jkhaimi <jesse.haimi@icloud.com>
2023-06-22 15:57:23 +0300

    test working ok

--- FrontEnd/my-app/src/views/ExampleTest.js
+++ FrontEnd/my-app/src/views/ExampleTest.js
@@ -1,23 +1,101 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import './ExampleTest.css';
+import jsonData from '../jsonFiles/testinfo.json';
+import SingleButtons from '../Questions/SingleButtons';
+import WhatsNext from '../Questions/WhatsNext';
 
-class ExampleTest extends Component {
-  constructor(props) {
-    super(props);
-    // Initialize state if needed
-    this.state = {};
-  }
+const ExampleTest = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [remainingTime, setRemainingTime] = useState(0.5 * 60); // 5 minutes in seconds
 
-  componentDidMount() {
-    // Perform any initialization or API calls after the component has mounted
-  }
+  const handleAnswerSubmit = (index, answer) => {
+    setUserAnswers((prevAnswers) => {
+      const updatedAnswers = [...prevAnswers];
+      updatedAnswers[index] = answer;
+      return updatedAnswers;
+    });
+  };
 
-  render() {
-    return (
-      <div className='exampleBody'>
-        {/* Add your JSX code here */}
+  const downloadUserAnswers = () => {
+    const userAnswersData = jsonData.map((questionData, index) => {
+      return {
+        id: questionData.id,
+        userAnswer: userAnswers[index],
+      };
+    });
+
+    const jsonDataString = JSON.stringify(userAnswersData, null, 2);
+
+    const link = document.createElement('a');
+    link.href = `data:text/json;charset=utf-8,${encodeURIComponent(jsonDataString)}`;
+    link.download = 'UserAnswers.json';
+    link.click();
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (remainingTime === 0) {
+      // Time's up, submit the answers
+      downloadUserAnswers();
+    }
+  }, [remainingTime]);
+
+  const minutes = Math.floor(remainingTime / 60);
+  const seconds = remainingTime % 60;
+
+  return (
+    <div className="app">
+      <div className="TopBar">
+      <div className="timer">
+        Time Remaining: {minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}
       </div>
-    );
-  }
-}
+        <h1 className="Test-Title">Welcome to the test</h1>
+      </div>
+      <div className="question-container">
+        {jsonData.map((questionData, index) => {
+          if (questionData.type === 'single') {
+            return (
+              <div key={index}>
+                <h1>{questionData.question}</h1>
+                <SingleButtons
+                  options={questionData.options}
+                  answer={questionData.answers}
+                  index={index}
+                  onSubmit={handleAnswerSubmit}
+                />
+              </div>
+            );
+          } else if (questionData.type === 'whatsNext') {
+            return (
+              <div key={index}>
+                <h1>{questionData.question}</h1>
+                <WhatsNext
+                  options={questionData.options}
+                  answer={questionData.answers}
+                  index={index}
+                  onSubmit={handleAnswerSubmit}
+                />
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <button className="submit-button" onClick={downloadUserAnswers}>
+        <strong>End test</strong>
+      </button>
+    </div>
+  );
+};
 
 export default ExampleTest;
--- FrontEnd/my-app/src/views/StartTest.js
+++ FrontEnd/my-app/src/views/StartTest.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import Clock from '../components/Clock';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './StartTest.css';
+import Clock from '../components/Clock';
 
+const StartTest = () => {
+  const [startTest, setStartTest] = useState(false);
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-class StartTest extends React.Component {
-  render() {
-    return (
-          <div className="Container">
-            <div className="TopBar">
-              <Clock />
-              <div className="StartTest-TextContainer">
-                <h1 className="Title">Math test 2nd grade Autumn</h1>
-                <p>Start the test by pressing the button below</p>
-              </div>
-            </div>
-            <div className='StartTestContainer'>
-              <h2 className='StartTestTitle'>The test is ready</h2>
-              <button className='StartTestButton'>Start test</button>
-            </div>
-          </div>
-    );
-  }
-}
+  const handleStartTest = () => {
+    if (password === 'kissa') {
+      setStartTest(true);
+      setErrorMessage('');
+    } else {
+      setErrorMessage('Invalid password. Please try again.');
+    }
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleStartTest();
+    }
+  };
+
+  return (
+    <div className="Container">
+      <div className="TopBar">
+        <Clock />
+        <div className="StartTest-TextContainer">
+          <h1 className="Title">Math test 2nd grade Autumn</h1>
+          <p>Start the test by entering the password and pressing the button below</p>
+        </div>
+      </div>
+      {startTest ? (
+        <Link to="/example-test" className="StartTestContainer">
+          <h2 className="StartTestTitle">The test is ready</h2>
+          <button className="StartTestButton">Start test</button>
+        </Link>
+      ) : (
+        <div className="StartTestContainer">
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            onKeyPress={handleKeyPress} // Add key press event handler
+            placeholder="Password"
+            className="PasswordInput"
+          />
+          <button className="StartTestButton" onClick={handleStartTest}>
+            Start test
+          </button>
+          {errorMessage && <p className="ErrorMessage">{errorMessage}</p>}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default StartTest;
