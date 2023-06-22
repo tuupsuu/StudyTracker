52e02d02c112f68ba336fe55e188959088d4d8b5
eepehukk <eemil.hukkanen01@gmail.com>
2023-06-22 08:39:43 +0300

    Making base for exampletest & examinetests

---
+++ FrontEnd/my-app/src/views/ExamineTests.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class ExamineTests extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize state if needed
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // Perform any initialization or API calls after the component has mounted
+  }
+
+  render() {
+    return (
+      <div className='examineBody'>
+        {/* Add your JSX code here */}
+      </div>
+    );
+  }
+}
+
+export default ExamineTests;
