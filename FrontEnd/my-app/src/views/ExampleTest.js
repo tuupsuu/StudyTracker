22737cad18979a57fce797983b05a6bea9da65d2
jkhaimi <jesse.haimi@icloud.com>
2023-06-28 10:18:13 +0300

    timed questions almost working

--- FrontEnd/my-app/src/views/ExampleTest.js
+++ FrontEnd/my-app/src/views/ExampleTest.js
@@ -1,14 +1,16 @@
-// ExampleTest.js
 import React, { useState, useEffect } from 'react';
 import './ExampleTest.css';
 import jsonData from '../jsonFiles/testinfo.json';
 import SingleButtons from '../Questions/SingleButtons';
 import WhatsNext from '../Questions/WhatsNext';
 import Audio from '../Questions/Audio';
+import TimedExercise from '../Questions/Timed';
 
 const ExampleTest = () => {
   const [userAnswers, setUserAnswers] = useState([]);
-  const [remainingTime, setRemainingTime] = useState(30 * 60); // 5 minutes in seconds
+  const [remainingTime, setRemainingTime] = useState(30 * 60); // Timer 30 * 60 seconds
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [showTimedExercise, setShowTimedExercise] = useState(false);
 
   const handleAnswerSubmit = (index, answer) => {
     setUserAnswers((prevAnswers) => {
@@ -23,21 +25,36 @@ const ExampleTest = () => {
   };
 
   const downloadUserAnswers = () => {
-    const userAnswersData = jsonData.map((questionData, index) => {
-      return {
-        id: questionData.id,
-        userAnswer: userAnswers[index],
-      };
-    });
+    if (showConfirmation) {
+      const userAnswersData = jsonData.map((questionData, index) => {
+        return {
+          id: questionData.id,
+          userAnswer: userAnswers[index],
+        };
+      });
 
-    const jsonDataString = JSON.stringify(userAnswersData, null, 2);
+      const jsonDataString = JSON.stringify(userAnswersData, null, 2);
 
-    const link = document.createElement('a');
-    link.href = `data:text/json;charset=utf-8,${encodeURIComponent(jsonDataString)}`;
-    link.download = 'UserAnswers.json';
-    link.click();
+      const link = document.createElement('a');
+      link.href = `data:text/json;charset=utf-8,${encodeURIComponent(jsonDataString)}`;
+      link.download = 'UserAnswers.json';
+      link.click();
 
-    redirectToMainMenu();
+      redirectToMainMenu();
+    }
+  };
+
+  const handleConfirmation = () => {
+    setShowConfirmation(true);
+  };
+
+  const handleCancelConfirmation = () => {
+    setShowConfirmation(false);
+  };
+
+  const handleConfirmSubmission = () => {
+    setShowConfirmation(false);
+    downloadUserAnswers();
   };
 
   useEffect(() => {
@@ -62,7 +79,7 @@ const ExampleTest = () => {
 
   return (
     <div className="test">
-      <div className="TopBar">
+      <div className="Test-TopBar">
         <div className="timer">
           Time Remaining: {minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}
         </div>
@@ -104,14 +121,43 @@ const ExampleTest = () => {
                 />
               </div>
             );
+          } else if (questionData.type === 'timer') {
+            return (
+              <div key={index}>
+                <h1>{questionData.question}</h1>
+                {!showTimedExercise ? (
+                  <button className="StartTimer-button" onClick={() => setShowTimedExercise(true)}>Start Timer</button>
+                ) : (
+                  <TimedExercise
+                    options={questionData.options}
+                    onSubmit={(answer) => handleAnswerSubmit(index, answer)}
+                  />
+                )}
+              </div>
+            );
           } else {
             return null;
           }
         })}
       </div>
-      <button className="submit-button" onClick={downloadUserAnswers}>
-        End Test
-      </button>
+      {!showConfirmation && (
+        <button className="submit-button" onClick={handleConfirmation}>
+          <strong>End Test</strong>
+        </button>
+      )}
+      {showConfirmation && (
+        <div className="confirmation-dialog">
+          <p className="confirmation-text">Are you sure you want to end the test?</p>
+          <div>
+            <button className="EndTestYes" onClick={handleConfirmSubmission}>
+              Yes
+            </button>
+            <button className="EndTestNo" onClick={handleCancelConfirmation}>
+              No
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
