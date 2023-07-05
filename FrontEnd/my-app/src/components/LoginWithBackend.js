3413d1c0cbc995c084e9408c1436402f528d4d1d
eepehukk <eemil.hukkanen01@gmail.com>
2023-07-05 14:49:24 +0300

    Docker stuff

--- FrontEnd/my-app/src/components/LoginWithBackend.js
+++ FrontEnd/my-app/src/components/LoginWithBackend.js
@@ -13,7 +13,7 @@ function LoginWithBackend() {
   const { setIsLoggedIn } = React.useContext(AuthContext);
 
   useEffect(() => {
-    fetch('http://userservicecontainer:3002/users')
+    fetch('http://userservicecontainer:3000/users')
       .then(response => response.json())
       .then(data => console.log(data))
       .catch(error => console.error('Error:', error));
