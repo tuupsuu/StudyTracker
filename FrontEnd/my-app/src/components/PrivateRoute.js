de4d90c5225f85ed9fa639ef8de64cbc4c8277cb
eepehukk <eemil.hukkanen01@gmail.com>
2023-07-17 16:28:42 +0300

    First version of the newly renovated privateroute

--- FrontEnd/my-app/src/components/PrivateRoute.js
+++ FrontEnd/my-app/src/components/PrivateRoute.js
@@ -1,16 +1,35 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from './AuthContext';
 
 export default function PrivateRoute({ children }) {
-    const { isLoggedIn } = useContext(AuthContext);
-    let navigate = useNavigate();
-    let location = useLocation();
+  const { isLoggedIn, setLoggedInStatus } = useContext(AuthContext);
+  let navigate = useNavigate();
+  let location = useLocation();
+  const [isChecking, setIsChecking] = useState(true);
 
-    if (!isLoggedIn) {
-        navigate("/", { replace: true, state: { from: location } });
-        return null;
+  useEffect(() => {
+    // check if token is available and not expired
+    const token = localStorage.getItem('jwtToken');
+    const expirationTime = localStorage.getItem('jwtTokenExpiration');
+
+    if (!token || !expirationTime || new Date().getTime() > expirationTime) {
+      setLoggedInStatus(false);
+      setIsChecking(false);
+    } else {
+      setLoggedInStatus(true);
+      setIsChecking(false);
     }
+  }, [setLoggedInStatus]);
 
-    return children;
+  if (isChecking) {
+    return null; // or render a loading spinner
+  }
+
+  if (!isLoggedIn) {
+    navigate("/", { replace: true, state: { from: location } });
+    return null;
+  }
+
+  return children;
 }
