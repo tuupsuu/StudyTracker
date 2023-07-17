7783b4d674c2e52793b22403c9559917e03159b4
eepehukk <eemil.hukkanen01@gmail.com>
2023-07-17 16:50:23 +0300

    First version of the newly renovated privateroute

--- FrontEnd/my-app/src/components/PrivateRoute.js
+++ FrontEnd/my-app/src/components/PrivateRoute.js
@@ -2,35 +2,35 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import AuthContext from './AuthContext';
 
-export default function PrivateRoute({ children }) {
-  const { isLoggedIn, setLoggedInStatus } = useContext(AuthContext);
-  let navigate = useNavigate();
-  let location = useLocation();
-  const [isChecking, setIsChecking] = useState(true);
-
-  useEffect(() => {
-    // check if token is available and not expired
-    const token = localStorage.getItem('jwtToken');
-    const expirationTime = localStorage.getItem('jwtTokenExpiration');
-
-    if (!token || !expirationTime || new Date().getTime() > expirationTime) {
-      setLoggedInStatus(false);
-      setIsChecking(false);
-    } else {
-      setLoggedInStatus(true);
-      setIsChecking(false);
+export default function PrivateRoute({ element }) {
+    const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+    let navigate = useNavigate();
+    let location = useLocation();
+    const [isChecking, setIsChecking] = useState(true);
+  
+    useEffect(() => {
+      // check if token is available and not expired
+      const token = localStorage.getItem('jwtToken');
+      const expirationTime = localStorage.getItem('jwtTokenExpiration');
+  
+      if (!token || !expirationTime || new Date().getTime() > expirationTime) {
+        setIsLoggedIn(false);
+        setIsChecking(false);
+      } else {
+        setIsLoggedIn(true);
+        setIsChecking(false);
+      }
+    }, [setIsLoggedIn]);
+  
+    if (isChecking) {
+      return null; // or render a loading spinner
     }
-  }, [setLoggedInStatus]);
-
-  useEffect(() => {
-    if (!isChecking && !isLoggedIn) {
+  
+    if (!isLoggedIn) {
       navigate("/", { replace: true, state: { from: location } });
+      return null;
     }
-  }, [isLoggedIn, isChecking, navigate, location]);
-
-  if (isChecking) {
-    return null; // or render a loading spinner
+  
+    return element;
   }
 
-  return children;
-}
