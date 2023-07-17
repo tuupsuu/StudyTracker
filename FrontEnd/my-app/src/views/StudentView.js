660846ea9068cb9f3ae20bb9245ebcccd2c0c9c9
Topias Liljegren <topias.liljegren@gmail.com>
2023-07-17 18:10:02 +0300

    logout troubleshooting

--- FrontEnd/my-app/src/views/StudentView.js
+++ FrontEnd/my-app/src/views/StudentView.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './StudentView.css';
 import Clock from '../components/Clock';
 import { BiLogOut } from 'react-icons/bi';
-import tests from '../jsonFiles/tests.json'; 
+import tests from '../jsonFiles/tests.json';
 
 function StudentView() {
   const [studentName, setStudentName] = useState('');
@@ -21,48 +21,30 @@ function StudentView() {
         localStorage.removeItem('jwtTokenExpiration');
         localStorage.removeItem('userRights');
         localStorage.removeItem('loggedInStudentName');
-        navigate("..");
+        navigate('..');
       }
     }, 1000); // checks every second
-  
+
     const loggedInStudentName = localStorage.getItem('loggedInStudentName');
     if (loggedInStudentName) {
       setStudentName(loggedInStudentName);
     }
 
-    // Define a variable to track whether the component is unmounting
-    let isUnmounting = false;
-
-    // Adding event listener for window/tab close
-    window.addEventListener('beforeunload', (ev) => {
-      if (!isUnmounting) {
-        ev.preventDefault();
-        localStorage.removeItem('jwtToken');
-        localStorage.removeItem('jwtTokenExpiration');
-        localStorage.removeItem('userRights');
-        localStorage.removeItem('loggedInStudentName');
-      }
-    });
-
-    // Remember to clear the interval when the component unmounts
-    return () => {
-      isUnmounting = true;
-      clearInterval(intervalId);
-      // Remove the event listener when the component unmounts
-      window.removeEventListener('beforeunload', (ev) => {
-        if (!isUnmounting) {
-          ev.preventDefault();
-          localStorage.removeItem('jwtToken');
-          localStorage.removeItem('jwtTokenExpiration');
-          localStorage.removeItem('userRights');
-          localStorage.removeItem('loggedInStudentName');
-        }
-      });
+    const handleBeforeUnload = (ev) => {
+      ev.preventDefault();
+      localStorage.removeItem('jwtToken');
+      localStorage.removeItem('jwtTokenExpiration');
+      localStorage.removeItem('userRights');
+      localStorage.removeItem('loggedInStudentName');
     };
 
-  }, [navigate]);
+    window.addEventListener('beforeunload', handleBeforeUnload);
 
-  
+    return () => {
+      clearInterval(intervalId);
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [navigate]);
 
   const getRandomColor = () => {
     const colors = ['pink', 'lightblue', 'lime', 'red', 'yellow'];
@@ -78,17 +60,23 @@ function StudentView() {
           <h1 className="Title">Welcome {studentName}!</h1>
           <p>You can choose your test from this page.</p>
         </div>
-        <Link to='..' className='Student-LogOutButton' onClick={() => {
-          localStorage.removeItem('jwtTokenExpiration');
-          localStorage.removeItem('jwtToken');
-          localStorage.removeItem('loggedInStudentName');
-        }}><BiLogOut></BiLogOut></Link>
-        </div>
-      <div className='TestOptions'>
-        {tests.map(test => (
-          <Link 
-            to={`/start-test`} 
-            className="TestContainerButton" 
+        <Link
+          to=".."
+          className="Student-LogOutButton"
+          onClick={() => {
+            localStorage.removeItem('jwtTokenExpiration');
+            localStorage.removeItem('jwtToken');
+            localStorage.removeItem('loggedInStudentName');
+          }}
+        >
+          <BiLogOut></BiLogOut>
+        </Link>
+      </div>
+      <div className="TestOptions">
+        {tests.map((test) => (
+          <Link
+            to={`/start-test`}
+            className="TestContainerButton"
             key={test.id}
             onClick={() => localStorage.setItem('currentTest', test.name)}
           >
