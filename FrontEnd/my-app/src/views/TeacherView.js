f2df9cd14bb6453c034ef99755a912d1d03346b1
eepehukk <eemil.hukkanen01@gmail.com>
2023-07-25 11:38:36 +0300

    testing for token expiration

--- FrontEnd/my-app/src/views/TeacherView.js
+++ FrontEnd/my-app/src/views/TeacherView.js
@@ -83,7 +83,7 @@ function TeacherView() {
         <header className="header">
           <FaBars className="hamburger" onClick={() => setSidebarOpen(true)} />
           <div className='HeaderTeacher'>
-            isTeacherNameLoaded && <h1 className='TitleTeacher'>Welcome, {teacherName}!</h1>
+            {isTeacherNameLoaded && <h1 className='TitleTeacher'>Welcome, {teacherName}!</h1>}
           </div>
           <Link to='..' className='LogoutButtonTeacher' onClick={() => {
             localStorage.removeItem("jwtToken");
